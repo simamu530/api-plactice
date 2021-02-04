db8a1d321b3a7526da5becc2c0b9dd71d23de64b
simamu530 <58772879+simamu530@users.noreply.github.com>
2021-02-04 15:59:04 +0900

    2

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
-import App from './App.vue'
+// import App from './App.vue'
 import router from './router'
 import Home from './views/Home.vue'
 
 Vue.use(VueRouter); //VueでRouterという昨日を使うというコマンド？
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  }
-];
+// const router = [
+//   {
+//     path: "./views",
+//     name: "Home",
+//     component: Home,
+//   }
+// ];
 
-const router = new VueRouter({
-  mode: "history",
-  base: ProcessingInstruction.env.Base_URL,
-  routes,
-}); //ここのコマンドはどういう意味があるのか
+// const router = new VueRouter({
+//   mode: "history",
+//   base: ProcessingInstruction.env.Base_URL,
+//   routes,
+// }); //ここのコマンドはどういう意味があるのか
 
 export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,14 @@
   <div class="home">
     <input type="text" v-model="box">
     <button @click="$router.push({ postnumber: 'Home' })" class="cp_btn">button</button>
-    <!-- pushの中がどういう意味かわかってません -->
+    <!-- pushの中がどういう意味かわかってません｜APIを利用うするうえで必要なものだとおもっていた -->
   </div>
 </template>
 
 <script>
 import axios from "axios"
 export default {
-  props: ["number"], //propsもどういう仕組かまだ理解できてないです
+  props: ["number"], //propsもどういう仕組かまだ理解できてないです｜今回使うファイルはAPP.vueとhome.vueと、main.jsの3つであってるか　appが親でhomeが子供だとおもっていて基本使うものだとおもっていた
   data() {
     return {
       box: "",
@@ -17,9 +17,12 @@ export default {
   },
   async created() {
     const item = await axios.get(
-      `https://https://apis.postcode-jp.com/api/v4/postcodekGyZ1L8sV96yHk6TCm2TNpBYxiaGTzi7PBNoXmZ` 
+      `https://apis.postcode-jp.com/api/v4/postcodes/1000001 \
+  -G -v \
+  -d "kGyZ1L8sV96yHk6TCm2TNpBYxiaGTzi7PBNoXmZ" ` 
     ); //apiのアドレス？がどのクエリを使うのか、どれを使うかをどうやって見極めるかがわかりません
     const numberData = item.data　//constの先からどう記入していいのかわからないです
+    console.log();
   }
 }
 
