d5b4d366e59dcb233357bf136661795b39ffcaca
simamu530 <58772879+simamu530@users.noreply.github.com>
2021-02-07 14:50:22 +0900

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="home">
     <input type="text" v-model="postCode">
     <button @click="getAdressPostCode" class="cp_btn">button</button>
-    <p>{{getCity}}</p>
+    <p>{{ getCity }}</p>
+    <p>{{ postCode }}</p>
     <!-- pushの中がどういう意味かわかってません｜APIを利用うするうえで必要なものだとおもっていた -->
   </div>
 </template>
@@ -16,15 +17,6 @@ export default {
       getCity:"",
     };
   },
-  async created() {
-    const item = await axios.get(
-      `https://apis.postcode-jp.com/api/v4/postcodes/1000001`
-    ); //apiのアドレス？がどのクエリを使うのか、どれを使うかをどうやって見極めるかがわかりません
-    const numberData = item.data
-    const postCode = item.city; //ここの三行追加してコンソールでまず中身が取り出せてるか確認したかったのですができてません。
-    console.log(postCode);
-    console.log(numberData);
-  },
   methods: {
     async getAdressPostCode() {
       const item = await axios.get(
@@ -33,9 +25,11 @@ export default {
     const numberData = item.data;
     console.log(numberData);
     console.log(this.postCode);
+    const getCity = item.data[0].city;
+    console.log(getCity);
     }
   },
 
-}
+};
 
 </script>
